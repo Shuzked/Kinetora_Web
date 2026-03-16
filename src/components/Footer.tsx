3913bd97220afb016c408192972fedbd147cb7c9
[dyad] <Shuzked@gmail.com>
2026-03-17 00:16:29 +0100

    [dyad] wrote 2 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -158,7 +158,7 @@ const Footer = () => {
                 <FaInstagram className="h-4 w-4" />
               </a>
             </div>
-            <div className="flex flex-col items-center gap-2 text-sm font-semibold text-[#F5F5F5]/80">
+            <div className="flex flex-col items-start gap-2 text-sm font-semibold text-[#F5F5F5]/80 text-left">
               {strings.legal.map((item) => (
                 <Link
                   key={item.to}
