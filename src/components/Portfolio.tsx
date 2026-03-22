d5e59f328947a6312ca13afa163acb32de1b9a4d
Shuzked <93399187+Shuzked@users.noreply.github.com>
2026-03-22 17:44:43 +0100

    feat(portfolio): center slider items on mobile viewports

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -116,18 +116,22 @@ const Portfolio = () => {
             grabCursor={true}
             slidesPerView={1.1}
             spaceBetween={16}
+            centeredSlides={true}
             breakpoints={{
               640: {
                 slidesPerView: 1.5,
                 spaceBetween: 20,
+                centeredSlides: true,
               },
               768: {
                 slidesPerView: 2.2,
                 spaceBetween: 24,
+                centeredSlides: false,
               },
               1024: {
                 slidesPerView: 3,
                 spaceBetween: 32,
+                centeredSlides: false,
               },
             }}
             className="w-full px-[5vw] lg:px-0 lg:max-w-7xl lg:mx-auto !overflow-visible"
