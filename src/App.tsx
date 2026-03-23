438b55e0904ff9ffc4e8ad1cae93fe1f0d808c0e
Shuzked <93399187+Shuzked@users.noreply.github.com>
2026-03-23 22:04:05 +0100

    Stability: Disable all automated reload guards (VersionWatcher & DynamicImportGuard)

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
           <Sonner />
           <BrowserRouter>
             {/* <VersionWatcher /> */}
-            <DynamicImportGuard />
+            {/* <DynamicImportGuard /> */}
             {!isMobile && <CustomCursor />}
             <CookieBanner />
             <SkipToContent />
